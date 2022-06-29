2bb7bddf3a4d7094b73e9930a5a7eb9168aeaf29
Harinilekkala <harini.lekkala1997@gmail.com>
2022-06-29 14:59:56 +0530

    [Harini]: Uc-1 Connecting Database

---
+++ EmployeePayroll_ADO/EmployeePayroll_ADO/EmployeeRepo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll_ADO
+{
+    public class EmployeeRepo
+    {
+        public static string connectionString = "Data Source = (localdb)\\MSSQLLocalDB;Initial Catalog = PAYROLL_SERVICE;";
+        SqlConnection connection = new SqlConnection(connectionString);
+        public DataSet Connectivity()
+        {
+            try
+            {
+                DataSet data = new DataSet();
+                using (this.connection)
+                {
+                    this.connection.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter("ConnectivityCheck", this.connection);
+                    adapter.Fill(data);
+                    this.connection.Close();
+                    Console.WriteLine("Connection Established");
+                    return data;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+    }
+}
---
+++ EmployeePayroll_ADO/EmployeePayroll_ADO/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace EmployeePayroll_ADO
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeeRepo getMethod = new EmployeeRepo();
+            getMethod.Connectivity();
+        }
+    }
+}
+
