2de45ed117dc039d28d3e96f1fc2ae5146534865
Harinilekkala <harini.lekkala1997@gmail.com>
2022-06-29 15:18:36 +0530

    [Harini]: UC-2 RetrieveData

--- EmployeePayroll_ADO/EmployeePayroll_ADO/EmployeePayroll_Model.cs
+++ EmployeePayroll_ADO/EmployeePayroll_ADO/EmployeePayroll_Model.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace EmployeePayroll_ADO
 {
-    internal class EmployeePayroll_Model
+    public class EmployeePayroll_Model
     {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double salary { get; set; }
+        public DateTime startDate { get; set; }
+        public string gender { get; set; }
+        public decimal mobile { get; set; }
+        public string address { get; set; }
+        public string department { get; set; }
+        public double basicPay { get; set; }
+        public double deductions { get; set; }
+        public double taxablePay { get; set; }
+        public double netPay { get; set; }
     }
 }
